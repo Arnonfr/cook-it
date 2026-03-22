d4ffce3022a639c533a57a47e74838f38c3940db
ארנון <hyh@MacBook-Air-sl-mspht.local>
2026-03-22 11:15:26 +0200

    Fix onDestroy visibility - must be public to override BridgeActivity

--- frontend/android/app/src/main/java/com/cookit/app/MainActivity.java
+++ frontend/android/app/src/main/java/com/cookit/app/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends BridgeActivity {
     }
 
     @Override
-    protected void onDestroy() {
+    public void onDestroy() {
         super.onDestroy();
         if (shareReceiver != null) {
             unregisterReceiver(shareReceiver);
